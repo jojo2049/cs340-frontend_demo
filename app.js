bd896cad5c93473259c621b6295ead4778aa6340
Sam Foltz <foltzs@oregonstate.edu>
2021-10-27 17:34:43 -0700

    Initial setup, serving static webpages

---
+++ app.js
@@ -0,0 +1,23 @@
+// == SETUP
+const path = require("path");
+const express = require("express");
+const app = express();
+const PORT = 5732;
+const ROOT = path.join(__dirname, "public");
+
+// == ROUTES
+let sendFile = file_name => (req, res) => res.sendFile(file_name, {root: ROOT});
+
+app.get("/",                    sendFile("index.html"));
+app.get("/home",                sendFile("index.html"));
+app.get("/users",               sendFile("users.html"));
+app.get("/recipes",             sendFile("recipes.html"));
+app.get("/food_items",          sendFile("food_items.html"));
+app.get("/genres",              sendFile("genres.html"));
+app.get("/ingredients_table",   sendFile("ingredients_table.html"));
+app.get("/genres_table",        sendFile("genres_table.html"));
+
+// == LISTENER
+app.listen(PORT, () => {
+    console.log("Express started on http://localhost:" + PORT + "; press Ctrl-C to terminate.");
+});
