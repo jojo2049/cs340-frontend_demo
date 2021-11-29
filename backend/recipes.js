63cd838979cb73fddc66a68f6ed260a946a84fcb
MooseOnTheRocks <sam.foltz@hotmail.com>
2021-11-29 00:54:10 -0800

    More refactoring, successfully converted recipes into backend

--- backend/recipes.js
+++ backend/recipes.js
@@ -1,40 +1,43 @@
-const { renderPartialHTML, query, renderTableData, extractValues, handler} = require("./common");
-
 function init(app, pool, hb) {
-    const recipesInsertKeys = ["user_id", "food_item_id", "quantity", "prep_time"];
-    const recipesInsertSQL = "INSERT INTO Recipes (user_id, food_item_id, quantity, prep_time) VALUES (?, ?, ?, ?);";
-    function recipesInsertSuccess(res, results) {
-        query(pool, "SELECT * FROM Recipes", [])
-        .then(rows => renderTableData(hb, rows, recipesInsertKeys, text => res.send(text)));
-    }
+    //== Initialize helper functions.
+    const { renderPartialHTML, query, renderTableData, handler} = require("./common").init(pool, hb);
 
+    //== Constants
+    const recipesKeys = ["user_id", "food_item_id", "quantity", "prep_time"];
+    const recipesSelectSQL = "SELECT * FROM Recipes";
+    const recipesInsertSQL = "INSERT INTO Recipes (user_id, food_item_id, quantity, prep_time) VALUES (?, ?, ?, ?);";
+    const recipesSelectGenreSQL = "SELECT Recipes.* FROM Recipes JOIN GenresTable ON GenresTable.genre_id = ? AND GenresTable.food_item_id = Recipes.food_item_id;";
+    
+    //== INSERT
+    const recipesInsertSuccess = (res, results) => query(pool, recipesSelectSQL, [])
+        .then(rows => renderTableData(rows, recipesKeys, text => res.send(text)));
+    const recipesInsertHandler = handler(recipesKeys, recipesInsertSQL, recipesInsertSuccess, logError);
+    app.post("/recipes/insert", recipesInsertHandler);
+    
+    //== SELECT BY GENRE
+    const recipesSelectGenreSuccess = (res, results) => renderTableData(results, recipesKeys, text => res.send(text));
+    const recipesSelectGenreHandler = handler(["genre_id"], recipesSelectGenreSQL, recipesSelectGenreSuccess, logError);
+    app.post("/recipes/select/genre", recipesSelectGenreHandler);
+
+    //== GET
+    const recipesGetSuccess = (res, rows) => {
+        let context = {
+            results_table: {headers: recipesKeys, rows},
+            scripts: ["recipes.js"],
+            layout: "query_interface"
+        };
+        renderPartialHTML("views/recipes.handlebars", context)
+        .then(html => res.send(html));
+    }
+    const recipesGetHandler = handler([], recipesSelectSQL, recipesGetSuccess, logError);
+    app.get("/recipes", recipesGetHandler);
+
+    //== Helper
     function logError(res, error) {
         console.log("Error: ");
         console.log(error);
         res.json(error);
     }
-
-    const recipesInsertHandler = handler(pool, recipesInsertKeys, recipesInsertSQL, recipesInsertSuccess, logError);
-    app.post("/recipes/insert", recipesInsertHandler);
-    
-
-    let recipesSelectGenreSQL = "SELECT Recipes.* FROM Recipes JOIN GenresTable ON GenresTable.genre_id = ? AND GenresTable.food_item_id = Recipes.food_item_id;";
-    function recipesSelectGenreSuccess(res, results) {
-        renderTableData(hb, results, recipesInsertKeys, text => res.send(text));
-    }
-    const recipesSelectByGenreHandler = handler(pool, ["genre_id"], recipesSelectGenreSQL, recipesSelectGenreSuccess, logError);
-    app.post("/recipes/select/genre", recipesSelectByGenreHandler);
-
-
-    function recipesGetSuccess(res, rows) {
-        let headers = recipesInsertKeys;
-        let context = { results_table: {headers, rows}, layout: "query_interface", scripts: ["recipes.js"] }
-        renderPartialHTML(hb, "views/recipes.handlebars", context)
-        .then(html => res.send(html));
-    }
-    const recipesGetHandler = handler(pool, [], "SELECT * FROM Recipes", recipesGetSuccess, logError);
-    app.get("/recipes", recipesGetHandler);
-
 
     console.log("Successfully init recipes.js");
 }
