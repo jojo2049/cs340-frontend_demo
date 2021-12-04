7e0859e26f7d8f32aed21cfbb7824104238c3417
Jonathan Luong <jonathanaluong@gmail.com>
2021-12-03 21:18:32 -0600

    refactored users backend

--- app.js
+++ app.js
@@ -47,51 +47,54 @@ ingredients_table.init(app, pool, handlebars);
 const genres = require("./backend/genres");
 genres.init(app, pool, handlebars);
 
-app.post('/users/insert', (req, res) => {
-    // SQL query.
-    // The '?' will be replaced, in order, with the values in the list passed to pool.query.
-    let sql = "INSERT INTO Users (first_name, last_name, email) VALUES (?, ?, ?);"
-    // Values to safely insert into the SQL query.
-    let values = [req.body["first_name"], req.body["last_name"], req.body["email"]];
-    pool.query(sql, values, (error, results, fields) => {
-        if (error) {
-            res.write(JSON.stringify(error));
-            res.end();
-            return;
-        }
-        res.json(results.json);
-    });
-});
+const users = require("./backend/users");
+users.init(app, pool, handlebars);
 
-// Using handlebars for rendering and query the mysql database.
-app.get("/users", (req, res) => {
-    // pool is the pooled connection to the CS340 database.
-    // Query the db for all users and provide a callback function.
-    // Callback is executed once the database responds to our query.
-    pool.query("SELECT * FROM Users", (error, results, fields) => {
-        // Something went wrong...
-        // Respond with the error and return.
-        if (error) {
-            res.write(JSON.stringify(error));
-            res.end();
-            return;
-        }
-        // At this point we got our results.
-        // Create an object called context.
-        let context = {};
-        // Assign some properties to it.
-        // Properties assigned to context are accessable within the template.
-        context.users = results;
-        context.title = "Users";
-        context.scripts = ["users.js"];
-        // res.render(template, context) where template is the
-        // name of the handlebars template file to be rendered.
-        // Note: don't write the file extension (.handlebars) just the name.
-        // Any properties assigned to context are accessable from
-        // the handlebars template using the {{prop}} syntax.
-        res.render("users", context);
-    });
-});
+// app.post('/users/insert', (req, res) => {
+//     // SQL query.
+//     // The '?' will be replaced, in order, with the values in the list passed to pool.query.
+//     let sql = "INSERT INTO Users (first_name, last_name, email) VALUES (?, ?, ?);"
+//     // Values to safely insert into the SQL query.
+//     let values = [req.body["first_name"], req.body["last_name"], req.body["email"]];
+//     pool.query(sql, values, (error, results, fields) => {
+//         if (error) {
+//             res.write(JSON.stringify(error));
+//             res.end();
+//             return;
+//         }
+//         res.json(results.json);
+//     });
+// });
+
+// // Using handlebars for rendering and query the mysql database.
+// app.get("/users", (req, res) => {
+//     // pool is the pooled connection to the CS340 database.
+//     // Query the db for all users and provide a callback function.
+//     // Callback is executed once the database responds to our query.
+//     pool.query("SELECT * FROM Users", (error, results, fields) => {
+//         // Something went wrong...
+//         // Respond with the error and return.
+//         if (error) {
+//             res.write(JSON.stringify(error));
+//             res.end();
+//             return;
+//         }
+//         // At this point we got our results.
+//         // Create an object called context.
+//         let context = {};
+//         // Assign some properties to it.
+//         // Properties assigned to context are accessable within the template.
+//         context.users = results;
+//         context.title = "Users";
+//         context.scripts = ["users.js"];
+//         // res.render(template, context) where template is the
+//         // name of the handlebars template file to be rendered.
+//         // Note: don't write the file extension (.handlebars) just the name.
+//         // Any properties assigned to context are accessable from
+//         // the handlebars template using the {{prop}} syntax.
+//         res.render("users", context);
+//     });
+// });
 
 app.post('/food_items/insert', (req, res) => {
     let sql = "INSERT INTO FoodItems (name, calorie) VALUES (?, ?);"
